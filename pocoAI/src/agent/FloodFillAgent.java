754e675bc462c7e0c6b12035b67900e9994bf85a
Missy-C <Supreme Overlord@Katsup>
2012-12-06 01:43:12 -0600

    WIP FloodFill. Because it hates me.

--- pocoAI/src/agent/FloodFillAgent.java
+++ pocoAI/src/agent/FloodFillAgent.java
@@ -1,6 +1,7 @@
 package agent;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -13,9 +14,12 @@ public class FloodFillAgent extends AbstractAgent {
 	
 	private Solution s;
 	private int[][] floodedBoard;
-	private Board state;
-	private boolean hasFoundGoal;
-
+	private Board originalState;
+	private Board currentState;
+	
+	private final int NOT_VISITED = -1;
+	private final int CANNOT_MOVE = Integer.MAX_VALUE;
+	
 	@Override
 	public String algorithmName() {
 		return "Flood Fill";
@@ -25,16 +29,19 @@ public class FloodFillAgent extends AbstractAgent {
 	protected Solution doFindSolution(Board startState) {
 		s = new Solution();
 		floodedBoard = new int[startState.getHeight()][startState.getWidth()];
-		state = startState;
+		originalState = startState;
+		currentState = startState.clone();
 		
 		Path best = new Path();
 		Location bestBox = null;
 		Map<Location, Path> boxToGoalPaths = new HashMap<Location, Path>();
 		
+//		s.appendAllActions(getPath(startState.getAgentLocation(), startState.getGoalLocation(), true).actions);
+		
 		// Get first box done
 		for (Location box : startState.getBoxLocations()) {
-			Path toBox = getPath(startState.getAgentLocation(), box);
-			Path toGoal = getPath(box, startState.getGoalLocation());
+			Path toBox = getPath(startState.getAgentLocation(), box, true);
+			Path toGoal = getPath(box, startState.getGoalLocation(), false);
 			Path totalPath = new Path(toBox, toGoal);
 			boxToGoalPaths.put(box, toGoal);
 			
@@ -48,7 +55,7 @@ public class FloodFillAgent extends AbstractAgent {
 		boxToGoalPaths.remove(bestBox);
 
 		// Rest of the boxes
-		while (startState.getBoxLocations().size() != 0) {
+		while (!boxToGoalPaths.isEmpty()) {
 			best = new Path();
 			
 			for (Map.Entry<Location, Path> boxPath : boxToGoalPaths.entrySet()) {
@@ -66,38 +73,184 @@ public class FloodFillAgent extends AbstractAgent {
 		return s;
 	}
 	
-	private Path getPath(Location from, Location to) {
-		hasFoundGoal = false;
-		floodFill(to, from, 0);
+	private Path getPath(Location from, Location to, boolean isToBox) {
+		floodFill(to, isToBox);
+		
+		// Debugging
+		for (int h = 0; h < floodedBoard.length; h++) {
+			for (int w = 0; w < floodedBoard[h].length; w++) {
+				System.out.print("[" + floodedBoard[h][w] + "]");
+			}
+			System.out.println();
+		}
 		
 		List<Action> actions = new LinkedList<Action>();
-		int moves = 0;
+		Location fromClone = new Location(from.getX(), from.getY());
+		
+		if (isToBox) {
+			actions.addAll(getActions(fromClone, 0, true));
+		} else {
+			// TODO need to make sure to find a path that is actually POSSIBLE
+			List<Action> boxActions = getActions(fromClone, -1, false);
+			
+			// Find actions for Agent
+			Iterator<Action> iter = boxActions.iterator();
+			while (iter.hasNext()) {
+				Action curAct = iter.next();
+				
+				// Use actions to act as what "side" the agent needs to be on to do the move
+				Action wantedSide = reverseAction(curAct);
+				Action currentSide = getAgentSide(from);
+				if (wantedSide != currentSide) {
+					int x = fromClone.getX() + wantedSide.getDX();
+					int y = fromClone.getY() + wantedSide.getDY();
+					boolean isReachable = floodFill(new Location(x, y), false);
+					
+					if (isReachable) {
+						actions.addAll(getActions(currentState.getAgentLocation(), -1, true));
+					} else {
+						// get possible locations for box
+						
+						// Move box so it is reachable
+						// Store actions
+						
+						x = fromClone.getX() + wantedSide.getDX();
+						y = fromClone.getY() + wantedSide.getDY();
+						floodFill(new Location(x, y), false);
+						actions.addAll(getActions(currentState.getAgentLocation(), -1, true));
+						
+						// get new actions
+					}
+					
+					// Add the act of moving the box
+					// NO THIS IS OLD SHIT AND WON'T WORK WITH NEW POSITION. FUCK YOU. JUST. FUCK. YOU.
+					actions.add(curAct);
+				}
+			}
+			
+		}
 		
 		// TODO think of way to handle actually pushing the box
 		
-		return new Path(actions, moves);
+		return new Path(actions, actions.size());
 	}
 	
-	private void floodFill(Location curr, Location goal, int dist) {
+	private List<Action> getActions(Location start, int end, boolean doMoveAgent) {
+		List<Action> actions = new LinkedList<Action>();
+		int wanted = floodedBoard[start.getY()][start.getX()] - 1;
+		while (wanted != end) {
+			Action nextAct = getNexAction(start, wanted);
+			actions.add(nextAct);
+			if (doMoveAgent) currentState.moveAgent(nextAct);
+			wanted--;
+			
+			// Update agent location
+			start.setX(start.getX() + nextAct.getDX());
+			start.setY(start.getY() + nextAct.getDY());
+		}
+		return actions;
+	}
+	
+	private Action getAgentSide(Location box) {
+		int agentX = currentState.getAgentLocation().getX();
+		int agentY = currentState.getAgentLocation().getY();
+		
+		int boxX = box.getX();
+		int boxY = box.getY();
+		
+		if (boxX - 1 == agentX && boxY == agentY) {
+			return Action.LEFT;
+		} else if (boxX + 1 == agentX && boxY == agentY) {
+			return Action.RIGHT;
+		} else if (boxX == agentX && boxY + 1 == agentY) {
+			return Action.DOWN;
+		} else {
+			return Action.UP;
+		}
+	}
+	
+	private List<Action> makeReachable(Location box, Action side) {
+		List<Action> actions = new LinkedList<Action>();
+		
+		switch (side) {
+		case DOWN:
+			if (currentState.canMoveAgent(Action.UP)) {
+				currentState.moveAgent(Action.UP);
+//				int x = start.getX() + nextAct.getDX());
+//				int y = start.getY() + nextAct.getDY());
+			}
+		}
+		
+		return actions;
+	}
+	
+	private boolean floodFill(Location goal, boolean boxIsGoal) {
+		// Set to constant
+		for (int h = 0; h < floodedBoard.length; h++) {
+			for (int w = 0; w < floodedBoard[h].length; w++) {
+				floodedBoard[h][w] = NOT_VISITED;
+			}
+		}
+		
+		int goalX = goal.getX();
+		int goalY = goal.getY();
+		if (currentState.isOutOfBounds(goalX, goalY) || currentState.isWall(goalX, goalY) ||
+				(!boxIsGoal && currentState.hasBox(goalX, goalY))) {
+			return false;
+		}
+		floodedBoard[goalY][goalX] = 0;
+		
+		int openCells = originalState.getHeight() * originalState.getWidth();
+		int curDist = 0;
+		while (openCells != 0) {
+			openCells--;
+			for (int h = 0; h < floodedBoard.length; h++) {
+				for (int w = 0; w < floodedBoard[h].length; w++) {
+					if (floodedBoard[h][w] == curDist) {
+						int newDist = curDist + 1;
+						
+						// Left
+						setFloodValue(w - 1, h, newDist);
+						
+						// Right
+						setFloodValue(w + 1, h, newDist);
+						
+						// Up
+						setFloodValue(w, h - 1, newDist);
+						
+						// Down
+						setFloodValue(w, h + 1, newDist);
+					}
+				}
+			}
+			curDist++;
+		}
+		return true;
+	}
+	
+	private void setFloodValue(int x, int y, int val) {
+		if (!currentState.isOutOfBounds(x, y) && floodedBoard[y][x] == NOT_VISITED) {
+			if (currentState.isWall(x, y) || currentState.hasBox(x, y)) {
+				floodedBoard[y][x] = CANNOT_MOVE;
+			} else {
+				floodedBoard[y][x] = val;
+			}
+		}
+	}
+	
+	private Action getNexAction(Location curr, int wanted) {
 		int x = curr.getX();
 		int y = curr.getY();
 		
-		if (x == goal.getX() && y == goal.getY()) {
-			hasFoundGoal = true;
-			return;
+		if (!currentState.isOutOfBounds(x + 1, y) && floodedBoard[y][x + 1] == wanted) {
+			return Action.RIGHT;
+		} else if (!currentState.isOutOfBounds(x - 1, y) && floodedBoard[y][x - 1] == wanted) {
+			return Action.LEFT;
+		} else if (!currentState.isOutOfBounds(x, y + 1) && floodedBoard[y + 1][x] == wanted) {
+			return Action.DOWN;
+		} else {
+			return Action.UP;
 		}
-		
-		if (state.isWall(x, y) || state.isOutOfBounds(x, y)) {
-			return;
-		}
-		
-		floodedBoard[y][x] = dist;
-		dist++;
-		
-		if (!hasFoundGoal) floodFill(new Location(x + 1, y), goal, dist);
-		if (!hasFoundGoal) floodFill(new Location(x, y + 1), goal, dist);
-		if (!hasFoundGoal) floodFill(new Location(x - 1, y), goal, dist);
-		if (!hasFoundGoal) floodFill(new Location(x, y - 1), goal, dist);
 	}
 	
 	private List<Action> getReversedActions(List<Action> acts) {
